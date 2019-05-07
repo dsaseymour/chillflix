32a30ebed9ceed10bab25107d2559ba4e473f2f7
Danny <dannyseymour1990@gmail.com>
2019-05-06 22:59:52 -0400

    Changes to support authentication with Azure AD B2C

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Auth from "./Auth";
 
 class App extends Component {
+  displayUserInformation() {
+    const auth = new Auth();
+    if (auth.isLoggedIn()) {
+      console.log(auth.currentUser());
+      return (
+        <div>
+          <p>
+            Welcome, {auth.currentUser().firstName}{" "}
+            {auth.currentUser().lastName} from {auth.currentUser().city},{" "}
+            {auth.currentUser().country}
+          </p>
+          <a className="App-link" href="/abc" onClick={() => auth.logout()}>
+            Sign Out
+          </a>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.displayUserInformation()}
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import b2cauth from "react-azure-adb2c";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+b2cauth.initialize({
+  instance: "https://login.microsoftonline.com/tfp/",
+  tenant: "chillflixtenant.onmicrosoft.com",
+  signInPolicy: "B2C_1_signupsign",
+  applicationId: "f1ceeaa5-60e3-47ad-a43e-2db09ce9e32c",
+  cacheLocation: "sessionStorage",
+  scopes: ["https://chillflixtenant.onmicrosoft.com/api/user_impersonation"],
+  redirectUri: "http://localhost:3000",
+  postLogoutRedirectUri: window.location.origin
+});
+
+b2cauth.run(() => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+  serviceWorker.unregister();
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
